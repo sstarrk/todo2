b0775f5ac72bc529519d00b0b2a5159bc2c97caa
sstarrk <170336793+sstarrk@users.noreply.github.com>
2024-11-08 13:50:31 +0500

    made classes for taks and projects

--- src/DOM.js
+++ src/DOM.js
@@ -1,1 +1,10 @@
+const newTaskBtn = document.querySelector(".new");
 const modal = document.querySelector("dialog");
+newTaskBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+const closeBtn = document.querySelector("#close");
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import "./styles.css"
 import "./DOM.js"
 
-const newTaskBtn = document.querySelector(".new");
-const modal = document.querySelector("dialog");
-newTaskBtn.addEventListener("click", () => {
-    modal.style.display = "flex";
-});
+class Project {
+    constructor(name) {
+        this._name = name;
+        this._tasks = [];
+    }
 
-const closeBtn = document.querySelector("#close");
-closeBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-});
+    get getName() {
+        return this._name;
+    }
+
+    get getList() {
+        return this._tasks;
+    }
+
+    set addTaskToList(task) {
+        this._tasks.push(task);
+    };
+};
+
+const allProjects = {
+    allTasks: new Project("Default"),
+    workPr: new Project("Work"),
+    personalPr: new Project("Personal"),
+    shoppingPr: new Project("Shopping"),
+    travelPr: new Project("Travel"),
+    homePr: new Project("Home"),
+    financePr: new Project("Finance")
+}
+
+class Task {
+    constructor(name, description, dueDate, priority, project) {
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.project = project;
+    }
+}
+
+
