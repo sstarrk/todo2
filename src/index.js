9a01fc679875159db4fefc203f9cca68f5b520a6
sstarrk <170336793+sstarrk@users.noreply.github.com>
2024-11-08 10:45:04 +0500

    Modal appears when click on task and closes when click close

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "./styles.css"
 import "./DOM.js"
+
+const newTaskBtn = document.querySelector(".new");
+const modal = document.querySelector("dialog");
+newTaskBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+const closeBtn = document.querySelector("#close");
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
